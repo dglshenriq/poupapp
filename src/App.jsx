5f6d49a3e19247476201ae4ee186e02e89c4f8b9
dglshenriq <dglsramone@gmail.com>
2026-09-01 11:54:12 -0300

    Cria container e inicia a estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import logo from './assets/logo.svg'
+import { Container } from './components/Container'
 
 function App() {
   return (
-    <>
+    <Container>
       <aside>
         <img src={logo} alt="" />
       </aside>
       <h1>Olá, seja bem-vindo(a) ao PoupApp!</h1>
-    </>
+    </Container>
   )
 }
 
