12f0e4b98e851ec9ed0ed51581d091fdf1fe4e3c
dglshenriq <dglsramone@gmail.com>
2026-09-04 18:15:32 -0300

    Estiliza o botao

--- src/components/Transactions/index.jsx
+++ src/components/Transactions/index.jsx
@@ -37,9 +37,11 @@ export const Transactions = () => {
           );
         })}
       </ul>
-      <Button>
-        <IconCurrency /> Adicionar transação
-      </Button>
+      <div className={styles.actions}>
+        <Button>
+          <IconCurrency /> Adicionar transação
+        </Button>
+      </div>
     </>
   );
 };
